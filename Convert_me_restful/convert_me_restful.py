38207d644ab781157fefa50cdee79cae78dcfc42
Jane Tan <jane@Janes-MacBook-Pro.local>
2021-11-22 16:49:52 -0800

    Change to args

--- Convert_me_restful/convert_me_restful.py
+++ Convert_me_restful/convert_me_restful.py
@@ -8,11 +8,11 @@ en_to_ch_translator = Translator(to_lang='zh', from_lang='en')
 
 @app.route('/convert_ch_to_en')
 def convert_ch_to_en():
-    txt = request.get_json()['word']
+    txt = request.args['word']
     return jsonify(ch_to_en_translator.translate(txt))
 
 
 @app.route('/convert_en_to_ch')
 def convert_en_to_ch():
-    txt = request.get_json()['word']
+    txt = request.args['word']
     return jsonify(en_to_ch_translator.translate(txt))
